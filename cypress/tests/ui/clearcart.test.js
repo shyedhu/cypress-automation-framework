418a0a64ef64eab59e128463a20aed38c683afbb
Admin <admin@Admins-MacBook-Pro.local>
2021-06-01 01:02:12 -0700

    Initial commit

---
+++ cypress/tests/ui/clearcart.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable  no-undef,import/extensions */
+import Custom from '../../utils/custom.js';
+
+const custom = new Custom();
+/**
+ * @description Test class to remove all added items from cart page
+ * @author Shyedhu
+ *
+ */
+describe('Remove added items from the cart page', () => {
+  beforeEach(() => {
+    cy.fixture('users.json').as('usersData');
+    custom.navigateTo(Cypress.env('ENV'));
+  });
+  it('remove items from cart page', () => {
+    cy.get('@usersData').then((usersData) => {
+      // validate the login
+      cy.login(usersData.username, usersData.pass);
+      // perform the add to cart
+      cy.addtocart();
+      // view the added items in cart page
+      cy.viewcart();
+      // remove added items from cart page
+      cy.clearcart();
+    });
+  });
+});
---
+++ cypress/tests/ui/login.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable  no-undef,import/extensions */
+import Custom from '../../utils/custom.js';
+
+const custom = new Custom();
+/**
+ * @description Test class to validate the login page
+ * @author Shyedhu
+ *
+ */
+describe('Validate the login page', () => {
+  beforeEach(() => {
+    cy.fixture('users.json').as('usersData');
+    custom.navigateTo(Cypress.env('ENV'));
+  });
+  it('test the login page', () => {
+    cy.get('@usersData').then((usersData) => {
+      // validate the login
+      cy.login(usersData.username, usersData.pass);
+      cy.logout();
+    });
+  });
+});
